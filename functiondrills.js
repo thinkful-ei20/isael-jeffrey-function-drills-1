020eb406a73bfc42bd35b1953bf68545727f89ac
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-02 12:53:06 -0500

    calculated yearOfBirth

--- functiondrills.js
+++ functiondrills.js
@@ -1,5 +1,7 @@
 function whoAmI(name, age) {
+  let yearOfBirth = 2018 - age;
   console.log('my name is Isael Lizama and I\'m 21 years old');
+  console.log('I was born in ' + yearOfBirth);
 }
 
 whoAmI("jeff", 33);
